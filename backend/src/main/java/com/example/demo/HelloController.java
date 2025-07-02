57ea8a09fe7bfbe9260436b723735bea2bef7aa2
zenitsu <kuyu0903@gmail.com>
2025-07-03 01:43:07 +0900

    プッシュテスト

--- backend/src/main/java/com/example/demo/HelloController.java
+++ backend/src/main/java/com/example/demo/HelloController.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(origins = "http://localhost:5173")
 public class HelloController {
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello from Spring Boot!";
-    }
+	@GetMapping("/hello")
+	public String hello() {
+
+		// 文字表示
+		String sampleDtring = "動作確認とプッシュテスト。　　";
+
+		return sampleDtring;
+	}
 }
